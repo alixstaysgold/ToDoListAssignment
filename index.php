59a4617c00d4807e8eea10344bfd93610dab09ac
Alix Most <alsaechao@mail.fhsu.edu>
2021-02-16 17:58:40 -0600

    final commit

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $newdesc = filter_input(INPUT_POST, "newdesc", FILTER_SANITIZE_STRING);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Alix's To Do List</title>
     <link rel="stylesheet" href="main.css">
 </head>
 
